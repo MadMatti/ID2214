e726f8b31051d473c858578579c3f6100b451fc1
Mattia Evangelisti <71966068+MadMatti@users.noreply.github.com>
2022-11-07 14:52:36 +0100

    working 1d

--- Assignment_1/assignment_1.py
+++ Assignment_1/assignment_1.py
@@ -7,29 +7,29 @@ def create_bins(df, nobins=10, bintype="equal-width"):
     df1 = df.copy()
     binning = {}
 
-    for col in df1:
-        if df1[col].dtype == "int64" or df1[col].dtype == "float64" and col not in ['CLASS', 'ID']:
-            if bintype == "equal-width":
-                df1[col], binning[col] = pd.cut(df1[col],nobins,labels=False, retbins=True)
-            elif bintype == "equal-size":
-                df1[col], binning[col] = pd.qcut(df[col],nobins, labels=False, retbins=True, duplicates='drop')
+    for col in df1.columns:
+        if col not in ['CLASS', 'ID']:
+            if df1[col].dtype == "int64" or df1[col].dtype == "float64":
+                if bintype == "equal-width":
+                    df1[col], binning[col] = pd.cut(df1[col],nobins,labels=False, retbins=True)
+                elif bintype == "equal-size":
+                    df1[col], binning[col] = pd.qcut(df[col],nobins, labels=False, retbins=True, duplicates='drop')
 
-            binning[col][0] = [-np.inf]
-            binning[col][-1] = [np.inf]
-            df1[col] = df1[col].astype('category')
-            df1[col] = df1[col].cat.set_categories([x for x in range(nobins)])
+                binning[col][0] = -np.inf
+                binning[col][-1] = np.inf
+                df1[col] = df1[col].astype('category')
+                df1[col] = df1[col].cat.set_categories([str(i) for i in df1[col].cat.categories], rename = True)
 
     return df1, binning
-
 
 def apply_bins(df, binning):
     df1 = df.copy()
 
     for col in df1:
         if col in binning.keys():
-            df1[col] = pd.cut(df1[col], binning[col])
-            df1[col].astype('category')
-            df1[col] = df1[col].cat.set_categories([x for x in range(len(binning.keys()))])
+            df1[col] = pd.cut(df1[col], binning[col], labels=False)
+            df1[col] = df1[col].astype('category')
+            df1[col] = df1[col].cat.set_categories([str(i) for i in df1[col].cat.categories], rename = True)
 
     return df1
 
