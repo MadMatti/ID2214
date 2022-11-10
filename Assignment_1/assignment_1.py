688cc02afe6466c4a3feb47f98d405e0e4d9298b
roelandhooijmans <roeland-hooijmans@hotmail.com>
2022-11-10 14:35:28 +0100

    added 2b (brier score)

--- Assignment_1/assignment_1.py
+++ Assignment_1/assignment_1.py
@@ -3,15 +3,23 @@ import numpy as np
 
 
 def brier_score(df, correctlabels):
+    
+    brier_score = 0
+    n = len(df.index)
+          
+    for i,p in df.iterrows():
+        
+        true_label = correctlabels[i]
 
-    obs_vector = []
-
-    for label in correctlabels:
-        index = df[label]
-        print(index)
-
-
-
+        o = np.zeros(len(p))
+        o[df.columns==true_label] = 1
+        
+        brier_score += ((p-o)**2).sum(axis=0)/n
+        
+    return brier_score
+        
+        
+        
 
 
 
@@ -19,6 +27,7 @@ def test():
     predictions = pd.DataFrame({"A":[0.5,0.5,0.5,0.25,0.25],"B":[0.5,0.25,0.25,0.5,0.25],"C":[0.0,0.25,0.25,0.25,0.5]})
 
     correctlabels = ["B","A","B","B","C"]
+    
 
     print("Brier score: {}".format(brier_score(predictions,correctlabels)))
 
