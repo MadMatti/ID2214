6ca64d157265169d85930b9a836e1851a8f5dfa1
Mattia Evangelisti <71966068+MadMatti@users.noreply.github.com>
2022-11-10 12:03:38 +0100

    Completed part 1

--- Assignment_1/assignment_1.py
+++ Assignment_1/assignment_1.py
@@ -1,42 +1,14 @@
 import pandas as pd
 import numpy as np
-from IPython.display import display
 
 
-def create_bins(df, nobins=10, bintype="equal-width"):
-    df1 = df.copy()
-    binning = {}
+def brier_score(df, correctlabels):
 
-    for col in df1.columns:
-        if col not in ['CLASS', 'ID']:
-            if df1[col].dtype == "int64" or df1[col].dtype == "float64":
-                if bintype == "equal-width":
-                    df1[col], binning[col] = pd.cut(df1[col],nobins,labels=False, retbins=True)
-                elif bintype == "equal-size":
-                    df1[col], binning[col] = pd.qcut(df[col],nobins, labels=False, retbins=True, duplicates='drop')
+    obs_vector = []
 
-                binning[col][0] = -np.inf
-                binning[col][-1] = np.inf
-                df1[col] = df1[col].astype('category')
-                df1[col] = df1[col].cat.set_categories([str(i) for i in df1[col].cat.categories], rename = True)
-
-    return df1, binning
-
-def apply_bins(df, binning):
-    df1 = df.copy()
-
-    for col in df1:
-        if col in binning.keys():
-            df1[col] = pd.cut(df1[col], binning[col], labels=False)
-            df1[col] = df1[col].astype('category')
-            df1[col] = df1[col].cat.set_categories([str(i) for i in df1[col].cat.categories], rename = True)
-
-    return df1
-
-
-
-
-
+    for label in correctlabels:
+        index = df[label]
+        print(index)
 
 
 
@@ -44,21 +16,19 @@ def apply_bins(df, binning):
 
 
 def test():
-    glass_train_df = pd.read_csv("Assignment_1/glass_train.csv")
+    predictions = pd.DataFrame({"A":[0.5,0.5,0.5,0.25,0.25],"B":[0.5,0.25,0.25,0.5,0.25],"C":[0.0,0.25,0.25,0.25,0.5]})
 
-    glass_test_df = pd.read_csv("Assignment_1/glass_test.csv")
+    correctlabels = ["B","A","B","B","C"]
 
-    glass_train_disc, binning = create_bins(glass_train_df,nobins=10,bintype="equal-size")
-    print("binning:")
-    for f in binning:
-        print("{}:{}".format(f,binning[f]))
+    print("Brier score: {}".format(brier_score(predictions,correctlabels)))
 
-    print()    
-    glass_test_disc = apply_bins(glass_test_df,binning)
-    display("glass_test_disc",glass_test_disc)
+test()
 
-if __name__ == "__main__":
-    test()
 
-    
+
+
+                
+
+
+
 
