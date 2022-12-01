590ae4c09d8c4d7482170fdc3aebddb6497f9496
Mattia Evangelisti <71966068+MadMatti@users.noreply.github.com>
2022-12-01 11:52:47 +0100

    Start 4th ass

---
+++ Assignment_4/data.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import numpy as np
+from IPython.display import display
+# import all rdkit needed libraries
+import rdkit
+from rdkit.Chem import AllChem
+from rdkit.Chem import Descriptors
+
+def load_data():
+    df = pd.read_csv('Assignment_4/Resources/training_smiles.csv')
+    return df
+
+def get_mol(df):
+    df['mol'] = df['SMILES'].apply(rdkit.Chem.MolFromSmiles)
+    return df
+
+def feature_extraction(df):
+    df['num_atoms'] = df['mol'].apply(lambda x: x.GetNumAtoms())
+    df['num_heavy_atoms'] = df['mol'].apply(lambda x: x.GetNumHeavyAtoms())
+    df['exact_mol_wt'] = df['mol'].apply(lambda x: Descriptors.ExactMolWt(x))
+    df['AI_COO'] = df['mol'].apply(lambda x: Descriptors.fr_Al_COO(x))
+    df['morgan_fp'] = df['mol'].apply(lambda x: AllChem.GetMorganFingerprintAsBitVect(x,2,nBits=124))
+
+    print(df.head())
+    return df
+
+
+    
+
+
+
+
+
+if __name__ == '__main__':
+    feature_extraction(get_mol(load_data()))
+    
